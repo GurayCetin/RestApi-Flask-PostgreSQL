c10a791acdb68956b8bdbf61d626615d9fd3d12d
GurayCetin <34660613+GurayCetin@users.noreply.github.com>
2019-01-14 23:41:28 +0300

    Add files via upload

---
+++ api.py
@@ -0,0 +1,78 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, request
+from datetime import datetime, date 
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+api = Api(app)
+
+app.config ['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost:5432/helloworld'
+db = SQLAlchemy(app)
+
+
+class Database(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique= True)
+    dateofBirth = db.Column(db.String(120))
+
+
+    def __init__(self,username,dateofBirth):
+        self.username = username
+        self.dateofBirth = dateofBirth
+
+class User(Resource):
+
+    def put(self, name):
+        
+        parser = reqparse.RequestParser()
+        parser.add_argument("dateofBirth")
+        args = parser.parse_args()
+        
+        user = db.session.query(Database).filter(Database.username == name).first()
+        
+        if user is None:
+            user = Database(name, args["dateofBirth"])
+            db.session.add(user)
+        else:
+            user.dateofBirth = args["dateofBirth"]
+
+        db.session.commit()
+
+        return '',204 
+
+
+    def get(self, name):
+
+        user = db.session.query(Database).filter(Database.username == name).first()
+        
+        birth = datetime.strptime(user.dateofBirth, "%Y-%m-%d").date()
+
+        today = date.today() 
+
+        nextbirthday = date(today.year,birth.month,birth.day)
+
+        result = (nextbirthday - today).days
+        
+        if (result < 0):
+            nextbirthday = date(today.year+1,birth.month,birth.day)
+            result = (nextbirthday - today).days
+
+        if (result > 0):
+            return {"message": "Hello "+user.username+"! Your birthday is in "+str(result)+" days"} 
+
+        elif (result == 0):
+            return {"message": "Hello "+user.username+"! Happy Birtday!"}
+
+        
+            
+db.create_all()
+
+api.add_resource(User, "/hello/<string:name>")
+
+app.run(debug=True)
+
+
+
+
