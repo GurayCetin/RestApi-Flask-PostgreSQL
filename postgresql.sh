b1ef60218f14ac571119ec0ccb1435edb59a9fc6
GurayCetin <34660613+GurayCetin@users.noreply.github.com>
2019-01-16 00:21:02 +0300

    Add files via upload

--- postgresql.sh
+++ postgresql.sh
@@ -1,33 +1,31 @@
 #!/bin/bash 
-
+#
 echo "*****************************************"
 echo "PostgreSQL installation"
 echo "*****************************************"
-
-sudo amazon-linux-extras install postgresql10=latest
-
+#
+sudo amazon-linux-extras install postgresql10=latest -y
+#
 sudo yum -y install postgresql postgresql-server postgresql-devel postgresql-contrib postgresql-docs
-
+#
 sudo postgresql-setup initdb
-
+#
 echo "*****************************************"
 echo "postgresql.conf update"
 echo "*****************************************"
-
+#
 sudo cp -r -v postgresql.conf /var/lib/pgsql/data/postgresql.conf
-
+#
 echo "*****************************************"
 echo "pg_hba.conf update"
 echo "*****************************************"
-
+#
 sudo cp -r -v pg_hba.conf /var/lib/pgsql/data/pg_hba.conf
-
+#
 echo "*****************************************"
 echo "database creation"
 echo "*****************************************"
-
-sudo -u postgres createdb helloworld
-
-
+#
 sudo service postgresql start
-
+#
+sudo -u postgres createdb helloworld
